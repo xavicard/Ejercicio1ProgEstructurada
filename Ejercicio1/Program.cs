e8b53d5301fa72a81b1ae9032496d54134c1d65b
Javier Cárdenas <javier.cardenas@ucatec.edu.bo>
2020-05-14 19:48:11 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
