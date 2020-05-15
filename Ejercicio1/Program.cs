ee200a9730a89b44c3f212888f00d3f077553104
xavi <javier.cardenas@ucatec.edu.bo>
2020-05-14 20:11:24 -0400

    Codigo para sumar 2 numeros

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Ejercicio1
 {
@@ -6,7 +7,16 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String n1,n2;
+            int num1, num2, suma;
+            Console.WriteLine("Ingrese el Numero 1");
+            n1 = Console.ReadLine();
+            Console.WriteLine("Ingrese el Numero 2");
+            n2 = Console.ReadLine();
+            num1 = int.Parse(n1);
+            num2 = int.Parse(n2);
+            suma = num1 + num2;
+            Console.WriteLine("La suma es "+suma);
         }
     }
 }
